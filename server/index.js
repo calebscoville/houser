d8017f77f50f560d686f7ff4f06fb5c2a168fc46
Caleb Scoville <calebscoville2010@gmail.com>
2019-05-24 14:02:32 -0600

    seventh

--- server/index.js
+++ server/index.js
@@ -1,2 +1,21 @@
+require("dotenv").config();
 const express = require('express')
-const Ctrl = require('.controller')
+const app = express()
+const port = 3456
+// const controller = require('controller')
+const session = require("express-session")
+
+let { SERVER_PORT, SESSION_SECRET } = process.env;
+
+app.use(express.json());
+app.use(
+  session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true
+  })
+);
+
+app.listen(SERVER_PORT, () => {
+  console.log(`Michelle is beautiful on ${SERVER_PORT}.`);
+});
